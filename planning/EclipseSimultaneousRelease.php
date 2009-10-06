2f4eaf354f0ada8595c2a740604424648215815c
david_williams <david_williams>
2009-10-06 03:29:37 +0000

    temporary home

--- planning/EclipseSimultaneousRelease.php
+++ planning/EclipseSimultaneousRelease.php
@@ -6,7 +6,7 @@
 ini_set("display_errors", "true");
 error_reporting (E_ALL);
 
-require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();  # include($App->getProjectCommon());  # All on the same line to unclutter the user's desktop'
+require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();  include($App->getProjectCommon());  # All on the same line to unclutter the user's desktop'
 
 $pageKeywords	= "Eclispe Simultaneous Release";
 $pageAuthor		= "David Williams";
