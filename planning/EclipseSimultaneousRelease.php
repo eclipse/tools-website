09cd75cfbfc7c887c5e444101b55746d6e7f7a52
david_williams <david_williams>
2009-10-06 03:34:55 +0000

    temporary home

--- planning/EclipseSimultaneousRelease.php
+++ planning/EclipseSimultaneousRelease.php
@@ -6,7 +6,7 @@
 ini_set("display_errors", "true");
 error_reporting (E_ALL);
 
-require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();  include($App->getProjectCommon());  # All on the same line to unclutter the user's desktop'
+require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();  #include($App->getProjectCommon());  # All on the same line to unclutter the user's desktop'
 
 $pageKeywords	= "Eclispe Simultaneous Release";
 $pageAuthor		= "David Williams";
@@ -38,6 +38,13 @@ if (file_exists($xhtmlFile)) {
 	$xmlbody=$xml->getElementsByTagName('body')->item(0);
 	$maincontent = $proc->transformToXML($xmlbody);
 	$html = <<<EOHTML
+	<!-- include empty lef nav col for now -->
+	<div id="leftcol">
+	<ul id="leftnav">
+	<li class="separator">&nbsp;</li>
+	</ul>
+
+	
 	<div id="maincontent">
 	<div id="midcolumn">$maincontent</div>
 	</div>
