70ef9d073a86313fb6f95508a23251335c588401
David Williams <david_williams@us.ibm.com>
2013-11-27 11:41:24 -0500

    Bug 266252 - update page Tools TL page at www.eclipse.org/tools Change theme from "Lazarus" to "Nova"

--- index.php
+++ index.php
@@ -1,40 +1,38 @@
 <?php  																														require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
 
-	#*****************************************************************************
-	#
-	# template.php
-	#
-	# Author: 		Denis Roy
-	# Date:			2005-06-16
-	#
-	# Description: Type your page comments here - these are not sent to the browser
-	#
-	#
-	#****************************************************************************
-	
-	#
-	# Begin: page-specific settings.  Change these. 
-	$pageTitle 		= "Eclipse Tools Project";
-	$pageKeywords	= "Eclipse Tools Project";
-	$pageAuthor		= "Tools PMC";
-	
-	# Add page-specific Nav bars here
-	# Format is Link text, link URL (can be http://www.someothersite.com/), target (_self, _blank), level (1, 2 or 3)
-	# $Nav->addNavSeparator("My Page Links", 	"downloads.php");
-	# $Nav->addCustomNav("My Link", "mypage.php", "_self", 3);
-	# $Nav->addCustomNav("Google", "http://www.google.com/", "_blank", 3);
+#*****************************************************************************
+#
+# template.php
+#
+# Author: 		Denis Roy
+# Date:			2005-06-16
+#
+# Description: Type your page comments here - these are not sent to the browser
+#
+#
+#****************************************************************************
 
-	# End: page-specific settings
-		
-	# Paste your HTML content between the EOHTML markers!	
-	$html = <<<EOHTML
+#
+# Begin: page-specific settings.  Change these. 
+$pageTitle 		= "Eclipse Tools Project";
+$pageKeywords	= "Eclipse Tools Project";
+$pageAuthor		= "Tools PMC";
+
+# Add page-specific Nav bars here
+# Format is Link text, link URL (can be http://www.someothersite.com/), target (_self, _blank), level (1, 2 or 3)
+# $Nav->addNavSeparator("My Page Links", 	"downloads.php");
+# $Nav->addCustomNav("My Link", "mypage.php", "_self", 3);
+# $Nav->addCustomNav("Google", "http://www.google.com/", "_blank", 3);
+
+# End: page-specific settings
+
+# Paste your HTML content between the EOHTML markers!	
+$html = <<<EOHTML
 
 <div id="maincontent">
-	<div id="midcolumn" style="width: 80%">
+        <div id="midcolumn" style="width: 80%">
 
-	<div class="homeitem" style="width: 97%">
-		<h3>About the Eclipse Tools Project</h3>
-		<ul>
+                <h3>About the Eclipse Tools Project</h3>
       <p>The Eclipse Tools Project is an open source project of the Eclipse Foundation. 
         It is overseen 
         by a <a href="http://projects.eclipse.org/projects/tools/developers">Project Management Committee</a> (PMC) 
@@ -47,13 +45,11 @@
       <p>Please see the <a href="http://projects.eclipse.org/projects/tools">project master list</a>
         for a list of all the exciting projects under the Tools PMC. Many of the best
         and most useful projects at Eclipse are here.</p>
-		</ul>
-	</div>
+        </div>
 
-		
-		<hr class="clearer" />
 
-	</div>
+                <hr class="clearer" />
+
 
 </div>
 
@@ -61,6 +57,6 @@
 EOHTML;
 
 
-	# Generate the web page
-	$App->generatePage($theme, $Menu, $Nav, $pageAuthor, $pageKeywords, $pageTitle, $html);
+# Generate the web page
+$App->generatePage($theme, $Menu, $Nav, $pageAuthor, $pageKeywords, $pageTitle, $html);
 ?>
