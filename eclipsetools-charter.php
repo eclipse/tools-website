55db12d8322b2ef2dc7600eda6effeec67b57877
David Williams <david_williams@us.ibm.com>
2014-06-26 06:17:30 -0400

    Bug 438206 - Convert tools (main) web page to solstice theme

--- eclipsetools-charter.php
+++ eclipsetools-charter.php
@@ -330,5 +330,5 @@ EOHTML;
 
 
 	# Generate the web page
-	$App->generatePage($theme, $Menu, $Nav, $pageAuthor, $pageKeywords, $pageTitle, $html);
+	$App->generatePage($theme, $Menu, NULL, $pageAuthor, $pageKeywords, $pageTitle, $html);
 ?>
