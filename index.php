aa48e1d5ac8e79a0376342b687b14245e6333199
David Williams <david_williams@us.ibm.com>
2014-06-26 04:34:11 -0400

    Bug 438206 - Convert tools (main) web page to solstice theme

--- index.php
+++ index.php
@@ -49,11 +49,10 @@ $html = <<<EOHTML
 </div>
 </div>
 
-<div id='rightcolumn'>
 <div class='sideitem'>
   <img src="EclipseToolsLogos/PNG/elipse-tools-logo128.png" alt="Eclipse Tools" height="128" width="128" style="float:right" /> 
 </div>
-</div>
+
 
 
 
